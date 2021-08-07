58128af5345e41da367a99c326af93cf97e973ab
MithunHub <mithun.mukherjee@outlook.com>
2021-08-07 07:31:25 -0700

    Add files via upload

---
+++ Prog01/rasp_pi.py
@@ -0,0 +1,179 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+#
+#    this is an UART-LoRa device and thers is an firmware on Module
+#    users can transfer or receive the data directly by UART and dont
+#    need to set parameters like coderate,spread factor,etc.
+#    |============================================ |
+#    |   It does not suport LoRaWAN protocol !!!   |
+#    | ============================================|
+#   
+#    This script is mainly for Raspberry Pi 3B+, 4B, and Zero series
+#    Since PC/Laptop does not have GPIO to control HAT, it should be configured by
+#    GUI and while setting the jumpers, 
+#    Please refer to another script pc_main.py
+#
+
+import sys
+import sx126x
+import threading
+import time
+import select
+import termios
+import tty
+from threading import Timer
+
+from time import strftime
+
+old_settings = termios.tcgetattr(sys.stdin)
+tty.setcbreak(sys.stdin.fileno())
+
+
+#
+#    Need to disable the serial login shell and have to enable serial interface 
+#    command `sudo raspi-config`
+#    More details: see https://github.com/MithunHub/LoRa/blob/main/Basic%20Instruction.md
+#
+#    When the LoRaHAT is attached to RPi, the M0 and M1 jumpers of HAT should be removed.
+#
+
+
+#    The following is to obtain the temprature of the RPi CPU 
+def get_cpu_temp():
+    tempFile = open( "/sys/class/thermal/thermal_zone0/temp" )
+    cpu_temp = tempFile.read()
+    tempFile.close()
+    return float(cpu_temp)/1000
+
+#   serial_num
+#       PiZero, Pi3B+, and Pi4B use "/dev/ttyS0"
+#
+#    Frequency is [850 to 930], or [410 to 493] MHz
+#
+#    address is 0 to 65535
+#        under the same frequence,if set 65535,the node can receive 
+#        messages from another node of address is 0 to 65534 and similarly,
+#        the address 0 to 65534 of node can receive messages while 
+#        the another note of address is 65535 sends.
+#        otherwise two node must be same the address and frequence
+#
+#    The tramsmit power is {10, 13, 17, and 22} dBm
+#
+#    RSSI (receive signal strength indicator) is {True or False}
+#        It will print the RSSI value when it receives each message
+#
+
+#node = sx126x.sx126x(serial_num = "/dev/ttyS0",freq=433,addr=30,power=22,rssi=False)
+node = sx126x.sx126x(serial_num = "/dev/ttyS0",freq=433,addr=80,power=22,rssi=True)
+
+def send_deal():
+    get_rec = ""
+    print("")
+    print("input a string such as \033[1;32m20,Hello World\033[0m,it will send `Hello World` to node of address 20 ",flush=True)
+    print("please input and press Enter key:",end='',flush=True)
+
+    while True:
+        rec = sys.stdin.read(1)
+        if rec != None:
+            if rec == '\x0a': break
+            get_rec += rec
+            sys.stdout.write(rec)
+            sys.stdout.flush()
+
+    get_t = get_rec.split(",")
+    
+    node.addr_temp = node.addr
+    node.set(node.freq,int(get_t[0]),node.power,node.rssi)
+    node.send(get_t[1])
+    time.sleep(0.2)
+    node.set(node.freq,node.addr_temp,node.power,node.rssi)
+
+    print('\x1b[2A',end='\r')
+    print(" "*100)
+    print(" "*100)
+    print(" "*100)
+    print('\x1b[3A',end='\r')
+
+
+def send_cpu_continue(send_to_who,continue_or_not = True):
+    
+    if continue_or_not:
+        global timer_task
+        global seconds
+        node.send_to = send_to_who
+        node.addr_temp = node.addr
+        node.set(node.freq,node.send_to,node.power,node.rssi)
+        day  = strftime("%d %m %y at %I:%M%p")
+        node.send(str(day)+" CPU Temperature:"+str(get_cpu_temp())+" C")
+        time.sleep(0.2)
+        node.set(node.freq,node.addr_temp,node.power,node.rssi)
+        timer_task = Timer(seconds,send_cpu_continue,(send_to_who,))
+        timer_task.start()
+    else:
+        node.send_to = send_to_who
+        node.addr_temp = node.addr
+        node.set(node.freq,node.send_to,node.power,node.rssi)
+        node.send("CPU Temperature:"+str(get_cpu_temp())+" C")
+        time.sleep(0.2)
+        node.set(node.freq,node.addr_temp,node.power,node.rssi)
+        timer_task.cancel()
+        pass
+    
+try:
+    time.sleep(1)
+    print("Press \033[1;32mEsc\033[0m to exit")
+    print("Press \033[1;32mi\033[0m   to send")
+    print("Press \033[1;32ms\033[0m   to send cpu temperature every 10 seconds")
+    #f=open("g.txt","a")
+    # it will send every seconds(default is 10) seconds 
+    # send_to_who is the address of other node ( defult is 100)
+    send_to_who = 21
+    seconds = 2
+    # timer_task = Timer(seconds,send_cpu_continue,(send_to_who,))
+    
+    while True:
+
+        if select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], []):
+            c = sys.stdin.read(1)
+
+            # dectect key Esc
+            if c == '\x1b':
+                #f.close()
+                break
+            # dectect key i
+            if c == '\x69':
+                send_deal()
+            # dectect key s
+            if c == '\x73':
+                print("Press \033[1;32mc\033[0m   to exit the send task")
+                timer_task = Timer(seconds,send_cpu_continue,(send_to_who,))
+                timer_task.start()
+                
+                while True:
+                    if sys.stdin.read(1) == '\x63':
+                        timer_task.cancel()
+                        print('\x1b[1A',end='\r')
+                        print(" "*100)
+                        print('\x1b[1A',end='\r')
+                        break
+
+            sys.stdout.flush()
+            
+            
+        node.receive()
+        
+        # timer,send messages automatically
+        
+except:
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+    # print('\x1b[2A',end='\r')
+    # print(" "*100)
+    # print(" "*100)
+    # print('\x1b[2A',end='\r')
+
+termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+# print('\x1b[2A',end='\r')
+# print(" "*100)
+# print(" "*100)
+# print('\x1b[2A',end='\r')
---
+++ Prog01/sx126x.py
@@ -0,0 +1,307 @@
+# This file is used for LoRa and Raspberry Pi4B/Pi Zero
+# This is the main class file that is often used for LoRa parameters settings
+
+import RPi.GPIO as GPIO
+import serial
+import time
+import datetime
+
+class sx126x:
+
+    M0 = 22
+    M1 = 27
+    # if the header is 0xC0, then the LoRa register settings dont lost when it poweroff, and 0xC2 will be lost.
+    # cfg_reg = [0xC0,0x00,0x09,0x00,0x00,0x00,0x62,0x00,0x17,0x00,0x00,0x00]
+    cfg_reg = [0xC2,0x00,0x09,0x00,0x00,0x00,0x62,0x00,0x17,0x00,0x00,0x00]
+    get_reg = bytes(12)
+    rssi = False
+    addr = 65535
+    serial_n = ""
+    send_to = 0
+    addr_temp = 0
+    freq = 868
+    power = 22
+    air_speed =2400
+
+    SX126X_UART_BAUDRATE_1200 = 0x00
+    SX126X_UART_BAUDRATE_2400 = 0x20
+    SX126X_UART_BAUDRATE_4800 = 0x40
+    SX126X_UART_BAUDRATE_9600 = 0x60
+    SX126X_UART_BAUDRATE_19200 = 0x80
+    SX126X_UART_BAUDRATE_38400 = 0xA0
+    SX126X_UART_BAUDRATE_57600 = 0xC0
+    SX126X_UART_BAUDRATE_115200 = 0xE0
+
+    SX126X_AIR_SPEED_300bps = 0x00
+    SX126X_AIR_SPEED_1200bps = 0x01
+    SX126X_AIR_SPEED_2400bps = 0x02
+    SX126X_AIR_SPEED_4800bps = 0x03
+    SX126X_AIR_SPEED_9600bps = 0x04
+    SX126X_AIR_SPEED_19200bps = 0x05
+    SX126X_AIR_SPEED_38400bps = 0x06
+    SX126X_AIR_SPEED_62500bps = 0x07
+
+    SX126X_PACKAGE_SIZE_240_BYTE = 0x00
+    SX126X_PACKAGE_SIZE_128_BYTE = 0x40
+    SX126X_PACKAGE_SIZE_64_BYTE = 0x80
+    SX126X_PACKAGE_SIZE_32_BYTE = 0xC0
+
+    SX126X_Power_22dBm = 0x00
+    SX126X_Power_17dBm = 0x01
+    SX126X_Power_13dBm = 0x02
+    SX126X_Power_10dBm = 0x03
+
+    def __init__(self,serial_num,freq,addr,power,rssi):
+        self.rssi = rssi
+        self.addr = addr
+        self.freq = freq
+        self.serial_n = serial_num
+        self.power = power
+        self.send_to = addr
+
+        # Initial the GPIO for M0 and M1 Pin
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(self.M0,GPIO.OUT)
+        GPIO.setup(self.M1,GPIO.OUT)
+        GPIO.output(self.M0,GPIO.LOW)
+        GPIO.output(self.M1,GPIO.HIGH)
+
+        # The hardware UART of Pi3B+,Pi4B is /dev/ttyS0
+        self.ser = serial.Serial(serial_num,9600)
+        self.ser.flushInput()
+        self.set(freq,addr,power,rssi)
+
+    def set(self,freq,addr,power,rssi,air_speed=2400,\
+            net_id=0,buffer_size = 240,crypt=0,\
+            relay=False,lbt=False,wor=False):
+        self.send_to = addr
+        self.addr = addr
+        # We should pull up the M1 pin when sets the module
+        GPIO.output(self.M0,GPIO.LOW)
+        GPIO.output(self.M1,GPIO.HIGH)
+        time.sleep(0.1)
+        low_addr = addr & 0xff
+        high_addr = addr >> 8 & 0xff
+        net_id_temp = net_id & 0xff
+        if freq > 850:
+            freq_temp = freq - 850
+        elif freq >410:
+            freq_temp = freq - 410
+
+        air_speed_temp = self.air_speed_cal(air_speed)
+        # if air_speed_temp != None:
+
+        buffer_size_temp = self.buffer_size_cal(buffer_size)
+        # if air_speed_temp != None:
+
+        power_temp = self.power_cal(power)
+        #if power_temp != None:
+
+        # At this moment there is a bug in get_channel_rssi() function with SX1268 HAT
+        # The get_channel_rssi() function works well in SX1262 HAT
+        # To solve the problem for SX1268, we use an alternative method to obtain RSSI value
+        # We enable the seventh bit of 06H register
+        # and obatin the RSSI value when the packet is received.
+
+        if rssi:
+            # if use get_channel_rssi() func,  then uncomment next line and comment next to next line!
+            # rssi_temp = 0x20
+            rssi_temp = 0x80
+        else:
+            rssi_temp = 0x00
+
+        l_crypt = crypt & 0xff
+        h_crypt = crypt >> 8 & 0xff
+
+        self.cfg_reg[3] = high_addr
+        self.cfg_reg[4] = low_addr
+        self.cfg_reg[5] = net_id_temp
+        self.cfg_reg[6] = self.SX126X_UART_BAUDRATE_9600 + air_speed_temp
+        #
+        # it will enable to read noise rssi value when add 0x20 as follow
+        #
+        self.cfg_reg[7] = buffer_size_temp + power_temp + 0x20
+        self.cfg_reg[8] = freq_temp
+        #
+        # it will output a packet rssi value following received message
+        # when enable seventh bit with 06H register(rssi_temp = 0x80)
+        #
+        self.cfg_reg[9] = 0x03 + rssi_temp
+        self.cfg_reg[10] = h_crypt
+        self.cfg_reg[11] = l_crypt
+        self.ser.flushInput()
+
+        for i in range(2):
+            self.ser.write(bytes(self.cfg_reg))
+            r_buff = 0
+            time.sleep(0.2)
+            if self.ser.inWaiting() > 0:
+                time.sleep(0.1)
+                r_buff = self.ser.read(self.ser.inWaiting())
+                if r_buff[0] == 0xC1:
+                    pass
+                    # print("parameters setting is :",end='')
+                    # for i in self.cfg_reg:
+                        # print(hex(i),end=' ')
+
+                    # print('\r\n')
+                    # print("parameters return is  :",end='')
+                    # for i in r_buff:
+                        # print(hex(i),end=' ')
+                    # print('\r\n')
+                else:
+                    pass
+                    #print("parameters setting fail :",r_buff)
+                break
+            else:
+                print("setting has been failed, trying again!")
+                self.ser.flushInput()
+                time.sleep(0.2)
+                print('\x1b[1A',end='\r')
+                if i == 1:
+                    print("setting fail, press Esc to exit and run again")
+                    time.sleep(2)
+                    print('\x1b[1A',end='\r')
+                pass
+
+        GPIO.output(self.M0,GPIO.LOW)
+        GPIO.output(self.M1,GPIO.LOW)
+        time.sleep(0.1)
+
+    def air_speed_cal(self,airSpeed):
+        air_speed_c = {
+            1200:self.SX126X_AIR_SPEED_1200bps,
+            2400:self.SX126X_AIR_SPEED_2400bps,
+            4800:self.SX126X_AIR_SPEED_4800bps,
+            9600:self.SX126X_AIR_SPEED_9600bps,
+            19200:self.SX126X_AIR_SPEED_19200bps,
+            38400:self.SX126X_AIR_SPEED_38400bps,
+            62500:self.SX126X_AIR_SPEED_62500bps
+        }
+        return air_speed_c.get(airSpeed,None)
+
+    def power_cal(self,power):
+        power_c = {
+            22:self.SX126X_Power_22dBm,
+            17:self.SX126X_Power_17dBm,
+            13:self.SX126X_Power_13dBm,
+            10:self.SX126X_Power_10dBm
+        }
+        return power_c.get(power,None)
+
+    def buffer_size_cal(self,bufferSize):
+        buffer_size_c = {
+            240:self.SX126X_PACKAGE_SIZE_240_BYTE,
+            128:self.SX126X_PACKAGE_SIZE_128_BYTE,
+            64:self.SX126X_PACKAGE_SIZE_64_BYTE,
+            32:self.SX126X_PACKAGE_SIZE_32_BYTE
+        }
+        return buffer_size_c.get(bufferSize,None)
+
+    def get_settings(self):
+        # the pin M1 of lora HAT must be high when enter setting mode and get parameters
+        GPIO.output(M1,GPIO.HIGH)
+        time.sleep(0.1)
+
+        # send command to get setting parameters
+        self.ser.write(bytes([0xC1,0x00,0x09]))
+        if self.ser.inWaiting() > 0:
+            time.sleep(0.1)
+            self.get_reg = self.ser.read(self.ser.inWaiting())
+
+        # check the return characters from HAT and print the setting parameters
+        if self.get_reg[0] == 0xC1 and self.get_reg[2] == 0x09:
+            fre_temp = self.get_reg[8]
+            addr_temp = self.get_reg[3] + self.get_reg[4]
+            air_speed_temp = self.get_reg[6] & 0x03
+            power_temp = self.get_reg[7] & 0x03
+
+            air_speed_dic = {
+                0x00:"300bps",
+                0x01:"1200bps",
+                0x02:"2400bps",
+                0x03:"4800bps",
+                0x04:"9600bps",
+                0x05:"19200bps",
+                0x06:"38400bps",
+                0x07:"62500bps"
+            }
+            power_dic ={
+                0x00:"22dBm",
+                0x01:"17dBm",
+                0x02:"13dBm",
+                0x03:"10dBm"
+            }
+
+            print("Frequence is {0}.125MHz.",fre_temp)
+            print("Node address is {0}.",addr_temp)
+            print("Air speed is "+ air_speed_dic(air_speed_temp))
+            print("Power is " + power_dic(power_temp))
+            GPIO.output(M1,GPIO.LOW)
+
+    def send(self,data):
+        GPIO.output(self.M1,GPIO.LOW)
+        GPIO.output(self.M0,GPIO.LOW)
+        time.sleep(0.1)
+
+        # add the node address ,and the node of address is 65535 can able to find who sends message
+        l_addr = self.addr_temp & 0xff
+        h_addr = self.addr_temp >> 8 & 0xff
+
+        self.ser.write(bytes([h_addr,l_addr])+data.encode())
+        # if self.rssi == True:
+            # self.get_channel_rssi()
+        time.sleep(0.1)
+
+    def receive(self):
+        if self.ser.inWaiting() > 0:
+            time.sleep(0.5)
+            r_buff = self.ser.read(self.ser.inWaiting())
+
+            print("receive message from address\033[1;32m %d node \033[0m"%((r_buff[0]<<8)+r_buff[1]),end='\r\n',flush = True)
+            print("message is "+str(r_buff[2:-1]),end='\r\n')
+
+            # print RSSI
+            if self.rssi:
+                # print('\x1b[3A',end='\r')
+                print("the packet rssi value: -{0}dBm".format(256-r_buff[-1:][0]))
+                # f=open("g.txt","a")
+                self.get_channel_rssi()
+                e = datetime.datetime.now()
+                f=open("g.txt","a")
+                f.write("Packet RSSI: -{0}dBm".format(256-r_buff[-1:][0]))
+                #f.write("a")
+                # print ("Current date and time = %s" % e)
+                f.write(" Current date and time = %s\n" % e)
+                f.close()
+
+            else:
+                pass
+                #print('\x1b[2A',end='\r')
+
+    def get_channel_rssi(self):
+        GPIO.output(self.M1,GPIO.LOW)
+        GPIO.output(self.M0,GPIO.LOW)
+        time.sleep(0.1)
+        self.ser.flushInput()
+        self.ser.write(bytes([0xC0,0xC1,0xC2,0xC3,0x00,0x02]))
+        time.sleep(0.5)
+        re_temp = bytes(5)
+        if self.ser.inWaiting() > 0:
+            time.sleep(0.1)
+            re_temp = self.ser.read(self.ser.inWaiting())
+        if re_temp[0] == 0xC1 and re_temp[1] == 0x00 and re_temp[2] == 0x02:
+            # print("the current noise rssi value: -{0}dBm".format(256-re_temp[3]))
+            f=open("g.txt","a")
+            print("Noise RSSI value: -{0}dBm".format(256-re_temp[3]))
+            f.write("Noise RSSI: -{0}dBm ".format(256-re_temp[3]))
+            f.close()
+        else:
+            # pass
+            print("Receive RSSI value failed!")
+            # print("receive rssi value fail: ",re_temp)
+
+    #def relay(self):
+    #def wor(self):
+    #def remote_config(self):
